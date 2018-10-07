5eaf16f9643458a67577d5fdc2587627a2c5e3b9
Sam Bonifacio <sam@bonifacio.name>
2018-10-07 16:18:21 +1100

    Add comment to decorator uclass

--- Plugins/RichTextBlockInlineDecorator/Source/RichTextBlockInlineDecorator/Public/RichTextBlockInlineDecorator.h
+++ Plugins/RichTextBlockInlineDecorator/Source/RichTextBlockInlineDecorator/Public/RichTextBlockInlineDecorator.h
@@ -5,6 +5,9 @@
 #include "Components/RichTextBlockDecorator.h"
 #include "RichTextBlockInlineDecorator.generated.h"
 
+/**
+ * Allows inline styling of text using <span> tags, supporting functionality that used to exist pre-4.20
+ */
 UCLASS()
 class URichTextBlockInlineDecorator : public URichTextBlockDecorator
 {
